e3a5d3bf4644d6d4643dd30cdf12fdf17ea00172
Sumin <ssudoku@icloud.com>
2024-08-28 19:33:56 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,7 +1,2 @@
 def solution(money):
-    mus = [] #[잔 수, 남는 돈] 
-
-    cups, last_money = divmod(money, 5500)
-    mus.append(cups)
-    mus.append(last_money)
-    return mus
+    return [money // 5500, money % 5500]
