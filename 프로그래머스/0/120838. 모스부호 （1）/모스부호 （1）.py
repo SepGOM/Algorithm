b9335619840a51557b6e063fa750fb3b75e243ce
Sumin <ssudoku@icloud.com>
2024-09-03 09:04:28 +0900

    [level 0] Title: 모스부호 (1), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,16 @@
+def solution(letter):
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    
+    letter_eng = ''
+    letter_mos = letter.split(" ")
+
+    for i in range(len(letter_mos)):
+        letter_eng += (morse[letter_mos[i]])
+
+    return letter_eng
