810b232379e5d7333a4a18921ad43abdcd5d07a8
Sumin <ssudoku@icloud.com>
2024-09-21 21:49:36 +0900

    [level 0] Title: 약수 구하기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = []
+    
+    for i in range(1, int(n ** 0.5) + 1):
+        if n % i == 0:
+            answer.append(i)
+            if (i ** 2) != n:
+                answer.append(n // i)
+                
+    answer.sort()
+    
+    return answer
