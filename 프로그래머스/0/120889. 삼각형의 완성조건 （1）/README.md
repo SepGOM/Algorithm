a400253e1adf5bf138f4c6a111b113f2c76dc606
Sumin <ssudoku@icloud.com>
2024-09-21 19:32:17 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/README.md
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 삼각형의 완성조건 (1) - 120889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120889#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120889) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 10.2 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 21일 19:30:19
+2024년 09월 21일 19:32:15
 
 ### 문제 설명
 
--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
@@ -1,19 +1,5 @@
 def solution(sides):
-    
-    if len(set(sides)) == 1:    #정삼각형
+    if max(sides) < (sum(sides) - max(sides)):
         return 1
-    elif len(set(sides)) == 2 & sides.count(max(sides)) == 2:   #이등변삼각형
-        return 1
-    else:   #삼각형
-        Max = max(sides)
-        SideSum = 0
-        
-        for i in sides:
-            if i != Max:
-                SideSum += i
-                
-        if Max < SideSum:
-            return 1
-        else:
-            return 2
- 
+    else:
+        return 2
