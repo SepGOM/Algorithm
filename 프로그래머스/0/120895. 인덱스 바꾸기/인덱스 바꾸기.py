a15f742e8cb7eebf9db2bbd141bec58bcce6e7ee
Sumin <ssudoku@icloud.com>
2024-09-21 18:54:09 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,7 +1,4 @@
 def solution(my_string, num1, num2):
-    string = list(map(str, my_string))
-    s = string[num1]
-    
-    string[num1], string[num2] = string[num2], s
-
+    string = list(map(str, my_string))   
+    string[num1], string[num2] = string[num2], string[num1]
     return ''.join(string) 
