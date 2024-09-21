54427f1f83058736f4716a2ce8d5cb70a336de24
Sumin <ssudoku@icloud.com>
2024-09-21 13:19:48 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        if i in ['a', 'e', 'i', 'o', 'u']:
+            my_string = my_string.replace(i,'')
+
+    answer = my_string
+    return answer
